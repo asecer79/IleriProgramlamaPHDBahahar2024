5571132a4e36084bb0fd9f7ac85e74dd122a9baa
asecer79 <asecer79@DESKTOP-TPOST9I>
2024-03-15 16:49:28 +0300

    u

--- W04/Polymorphism/Vehicle.cs
+++ W04/Polymorphism/Vehicle.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace W04.Polymorphism
+﻿namespace W04.Polymorphism
 {
     public abstract class Vehicle
     {
